8ef132b45ccb38f2e2179b8b0ff5097780713aac
Jerónimo Taborda <jeronimo.taborda63057@ucaldas.edu.co>
2025-10-02 15:45:30 -0500

    Cambios

--- HeritanceWorksheet/View/TestMenu.java
+++ HeritanceWorksheet/View/TestMenu.java
@@ -5,9 +5,9 @@ public class TestMenu {
         Console keyboard = System.console();
 
         String id;
-        int option, optionShape;
+        int option;
         double x, y, firstSide, secondSide, thirdSide, mayorAxis, minorAxis;
-        boolean menu = true;
+        boolean menu = true, condition;
 
         ShapeList shapes = new ShapeList();
         Square square;
@@ -19,75 +19,140 @@ public class TestMenu {
         while (menu) {
             System.out.println(
                     "\n- - - - - - - - - - MENU DE FIGURAS - - - - - - - - - -\n- Ingrese una opción\n (1) Crear nueva figura\n (2) Mostrar lista de figuras\n (3) Consultar figura\n (4) Calcular área y perimetro de todas las figuras\n- Pulse otro número para salir del programa");
-            option = Integer.parseInt(keyboard.readLine());
+            try {
+                option = Integer.parseInt(keyboard.readLine());
+            } catch (Exception e) {
+                option = -1;
+            }
             switch (option) {
                 case 1 -> {
                     System.out.println(
                             "Ingrese:\n (1) Crear cuadrado\n (2) Crear rectángulo\n (3) Crear círculo\n (4) Crear triángulo\n (5) Crear óvalo");
-                    optionShape = Integer.parseInt(keyboard.readLine());
-                    switch (optionShape) {
+                    try {
+                        option = Integer.parseInt(keyboard.readLine());
+                    } catch (Exception e) {
+                        option = -1;
+                    }
+                    switch (option) {
                         case 1 -> {
+                            condition = false;
                             id = keyboard.readLine("\n- Ingrese el ID del cuadrado: ");
-                            x = Double.parseDouble(keyboard.readLine(
-                                    "\n- Ingrese la coordenada en el eje X del cuadrado: "));
-                            y = Double.parseDouble(keyboard.readLine(
-                                    "\n- Ingrese la coordenada en el eje Y del cuadrado: "));
-                            firstSide = Double.parseDouble(keyboard.readLine(
-                                    "\n- Ingrese el lado del cuadrado (cm): "));
-                            shapes.addShape(new Square(id, x, y, firstSide));
+                            for (Shape shape : shapes.getShapeList()) {
+                                if (shape.getId().equals(id)) {
+                                    condition = true;
+                                }
+                            }
+                            if (condition == true) {
+                                System.out.println(
+                                        "\n- Ya hay una figura existente con el mismo nombre. Volviendo al menú...");
+                            } else {
+                                x = Double.parseDouble(keyboard.readLine(
+                                        "\n- Ingrese la coordenada en el eje X del cuadrado: "));
+                                y = Double.parseDouble(keyboard.readLine(
+                                        "\n- Ingrese la coordenada en el eje Y del cuadrado: "));
+                                firstSide = Double.parseDouble(keyboard.readLine(
+                                        "\n- Ingrese el lado del cuadrado (cm): "));
+                                shapes.addShape(new Square(id, x, y, firstSide));
+                            }
                         }
                         case 2 -> {
+                            condition = false;
                             id = keyboard.readLine("\n- Ingrese el ID del rectángulo: ");
-                            x = Double.parseDouble(keyboard.readLine(
-                                    "\n- Ingrese la coordenada en el eje X del rectángulo: "));
-                            y = Double.parseDouble(keyboard.readLine(
-                                    "\n- Ingrese la coordenada en el eje Y del rectángulo: "));
-                            firstSide = Double.parseDouble(keyboard.readLine(
-                                    "\n- Ingrese la base del rectángulo (cm): "));
-                            secondSide = Double.parseDouble(keyboard.readLine(
-                                    "\n- Ingrese la altura del rectángulo: "));
-                            shapes.addShape(new Rectangle(id, x, y, firstSide, secondSide));
+                            for (Shape shape : shapes.getShapeList()) {
+                                if (shape.getId().equals(id)) {
+                                    condition = true;
+                                }
+                            }
+                            if (condition == true) {
+                                System.out.println(
+                                        "\n- Ya hay una figura existente con el mismo nombre. Volviendo al menú...");
+                            } else {
+                                x = Double.parseDouble(keyboard.readLine(
+                                        "\n- Ingrese la coordenada en el eje X del rectángulo: "));
+                                y = Double.parseDouble(keyboard.readLine(
+                                        "\n- Ingrese la coordenada en el eje Y del rectángulo: "));
+                                firstSide = Double.parseDouble(keyboard.readLine(
+                                        "\n- Ingrese la base del rectángulo (cm): "));
+                                secondSide = Double.parseDouble(keyboard.readLine(
+                                        "\n- Ingrese la altura del rectángulo: "));
+                                shapes.addShape(new Rectangle(id, x, y, firstSide, secondSide));
+                            }
                         }
                         case 3 -> {
+                            condition = false;
                             id = keyboard.readLine("\n- Ingrese el ID del círculo: ");
-                            x = Double.parseDouble(keyboard.readLine(
-                                    "\n- Ingrese la coordenada en el eje X del círculo: "));
-                            y = Double.parseDouble(keyboard.readLine(
-                                    "\n- Ingrese la coordenada en el eje Y del círculo: "));
-                            mayorAxis = Double.parseDouble(keyboard.readLine(
-                                    "\n- Ingrese el radio del círculo (cm): "));
-                            shapes.addShape(new Circle(id, x, y, mayorAxis));
+                            for (Shape shape : shapes.getShapeList()) {
+                                if (shape.getId().equals(id)) {
+                                    condition = true;
+                                }
+                            }
+                            if (condition == true) {
+                                System.out.println(
+                                        "\n- Ya hay una figura existente con el mismo nombre. Volviendo al menú...");
+                            } else {
+                                x = Double.parseDouble(keyboard.readLine(
+                                        "\n- Ingrese la coordenada en el eje X del círculo: "));
+                                y = Double.parseDouble(keyboard.readLine(
+                                        "\n- Ingrese la coordenada en el eje Y del círculo: "));
+                                mayorAxis = Double.parseDouble(keyboard.readLine(
+                                        "\n- Ingrese el radio del círculo (cm): "));
+                                shapes.addShape(new Circle(id, x, y, mayorAxis));
+                            }
 
                         }
                         case 4 -> {
-                            // TRIANGLE
+                            condition = false;
                             id = keyboard.readLine("\n- Ingrese el ID del triángulo: ");
-                            x = Double.parseDouble(keyboard.readLine(
-                                    "\n- Ingrese la coordenada en el eje X del triángulo: "));
-                            y = Double.parseDouble(keyboard.readLine(
-                                    "\n- Ingrese la coordenada en el eje Y del triángulo: "));
-                            firstSide = Double.parseDouble(keyboard.readLine(
-                                    "\n- Ingrese el primer lado del triángulo (cm): "));
-                            secondSide = Double.parseDouble(keyboard.readLine(
-                                    "\n- Ingrese el segundo lado del triángulo (cm): "));
-                            thirdSide = Double.parseDouble(keyboard.readLine(
-                                    "\n- Ingrese el tercer lado del triángulo (cm): "));
-                            shapes.addShape(new Triangle(id, x, y, firstSide, secondSide,
-                                    thirdSide));
-
+                            for (Shape shape : shapes.getShapeList()) {
+                                if (shape.getId().equals(id)) {
+                                    condition = true;
+                                }
+                            }
+                            if (condition == true) {
+                                System.out.println(
+                                        "\n- Ya hay una figura existente con el mismo nombre. Volviendo al menú...");
+                            } else {
+                                x = Double.parseDouble(keyboard.readLine(
+                                        "\n- Ingrese la coordenada en el eje X del triángulo: "));
+                                y = Double.parseDouble(keyboard.readLine(
+                                        "\n- Ingrese la coordenada en el eje Y del triángulo: "));
+                                firstSide = Double.parseDouble(keyboard.readLine(
+                                        "\n- Ingrese el primer lado del triángulo (cm): "));
+                                secondSide = Double.parseDouble(keyboard.readLine(
+                                        "\n- Ingrese el segundo lado del triángulo (cm): "));
+                                thirdSide = Double.parseDouble(keyboard.readLine(
+                                        "\n- Ingrese el tercer lado del triángulo (cm): "));
+                                triangle = new Triangle(id, x, y, firstSide, secondSide,
+                                        thirdSide);
+                                if (triangle.isValide(triangle)) {
+                                    shapes.addShape(triangle);
+                                } else {
+                                    System.out.println("\n- Triángulo no válido. Volviendo al menú...");
+                                }
+                            }
                         }
                         case 5 -> {
-                            // OVAL
+                            condition = false;
                             id = keyboard.readLine("\n- Ingrese el ID del óvalo:");
-                            x = Double.parseDouble(keyboard.readLine(
-                                    "\n- Ingrese la coordenada en el eje X del óvalo: "));
-                            y = Double.parseDouble(keyboard.readLine(
-                                    "\n- Ingrese la coordenada en el eje Y del óvalo: "));
-                            mayorAxis = Double.parseDouble(keyboard.readLine(
-                                    "\n- Ingrese el eje mayor del óvalo (cm): "));
-                            minorAxis = Double.parseDouble(keyboard.readLine(
-                                    "\n- Ingrese el eje menor del óvalo (cm): "));
-                            shapes.addShape(new Oval(id, x, y, minorAxis, mayorAxis));
+                            for (Shape shape : shapes.getShapeList()) {
+                                if (shape.getId().equals(id)) {
+                                    condition = true;
+                                }
+                            }
+                            if (condition == true) {
+                                System.out.println(
+                                        "\n- Ya hay una figura existente con el mismo nombre. Volviendo al menú...");
+                            } else {
+                                x = Double.parseDouble(keyboard.readLine(
+                                        "\n- Ingrese la coordenada en el eje X del óvalo: "));
+                                y = Double.parseDouble(keyboard.readLine(
+                                        "\n- Ingrese la coordenada en el eje Y del óvalo: "));
+                                mayorAxis = Double.parseDouble(keyboard.readLine(
+                                        "\n- Ingrese el eje mayor del óvalo (cm): "));
+                                minorAxis = Double.parseDouble(keyboard.readLine(
+                                        "\n- Ingrese el eje menor del óvalo (cm): "));
+                                shapes.addShape(new Oval(id, x, y, minorAxis, mayorAxis));
+                            }
                         }
                         default -> {
                             System.out.println(
